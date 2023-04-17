2e35fb66f16597cf04ad4e51a705ae46806cb74c
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-17 14:09:06 +0700

    Update Plants.java

--- Plants/Plants.java
+++ Plants/Plants.java
@@ -1,27 +1,34 @@
 package Plants;
 
-import java.awt.Image;
 import java.awt.Point;
 
-import Others.ObjectDrag;
+import javax.swing.ImageIcon;
 
-public class Plants extends ObjectDrag{
+public class Plants {
     private int plantDamage;
     private int plantHealth;
     private int xCoordinate;
     private int yCoordinate;
     protected static int plantCount = 0;
-    Image image;
-    public Point prePt = new Point(xCoordinate,yCoordinate);
-    public Point currentPt;
+    
+    public ImageIcon image;
+    public int WIDTH;
+    public int HEIGHT;
+    public ImageIcon cardImage;
+    public Point imageCorner;
+    public Point imageFirstPoint;
 
-    public Plants(int plantDamage,int plantHealth,int x,int y,Image image){
-        super(x,y,image);
+    public Plants(int plantDamage,int plantHealth,int x,int y,ImageIcon image,ImageIcon cardImage){
         this.plantDamage = plantDamage;
         this.plantHealth = plantHealth;
         this.xCoordinate = x;
         this.yCoordinate = y;
         this.image = image;
+        this.cardImage = cardImage;
+        this.imageCorner = new Point(x,y);
+        this.imageFirstPoint = new Point(x,y);
+        this.WIDTH = cardImage.getIconWidth();
+        this.HEIGHT = cardImage.getIconHeight();
         plantCount++;
     }
 
@@ -33,7 +40,12 @@ public class Plants extends ObjectDrag{
         return yCoordinate;
     }
 
-    public Image getImage(){
+    public ImageIcon getImage(){
         return image;
     }
+
+    public ImageIcon getCardImage(){
+        return cardImage;
+    }
+
 }
