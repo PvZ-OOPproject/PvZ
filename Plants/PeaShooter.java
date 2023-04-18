6c1b0759ae47a5aaca2fe948027bd21e979ea9a7
ChocoPieee <128669640+ChocoPieee@users.noreply.github.com>
2023-04-18 21:37:59 +0700

    Update PeaShooter.java

--- Plants/PeaShooter.java
+++ Plants/PeaShooter.java
@@ -1,16 +1,38 @@
 package Plants;
 
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+
 import javax.swing.ImageIcon;
+
+import Control.GamePanel;
+
 
 public class PeaShooter extends Plants{
     private static int countPeaShooter;
+    public static ArrayList<Pea> peaList;
    
     public PeaShooter(int plantDamage, int plantHealth,int x,int y) {
-        super(plantDamage, plantHealth,x,y,new ImageIcon("pea_shooter.gif"),new ImageIcon("pea_shooter_price.png"));
+        super(plantDamage, plantHealth,x,y,new ImageIcon("pea_shooter.gif"),new ImageIcon("pea_shooter_price.png"),"PeaShooter");
+        peaList = new ArrayList<Pea>();
         countPeaShooter++;
     }
 
     public int getCountNormal(){
         return countPeaShooter;
-    }    
+    }
+
+    public void addPea(Pea pea){
+        peaList.add(pea);
+    }
+
+    public void drawPea(GamePanel panel,Graphics g){
+        for(Pea i : peaList){
+            //i.setX((int) this.currentPoint.getX());
+            i.image.paintIcon(panel,g,i.getX(),(int) this.currentPoint.getY());
+        }
+    }
+
+
 }
