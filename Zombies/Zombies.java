174ef177152547523c5fe859fa221c81e5c99a3e
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-20 11:57:44 +0700

    Update Zombies.java

--- Zombies/Zombies.java
+++ Zombies/Zombies.java
@@ -1,21 +1,25 @@
 package Zombies;
 
 import java.awt.Image;
+import Others.Projectile;
+import Plants.Pea;
 
 public class Zombies {
+    private int zombieDamage;
     private int zombieSpeed;
     private int zombieHealth;
     private int xCoordinate;
     private int yCoordinate;
     protected static boolean gameOver = false;
     protected static int zombieCount = 0;
-    Image image;
+    ImageIcon image;
 
-    public Zombies(int zombieSpeed,int zombieHealth,int xCoordinate,int yCoordinate,Image image){
+    public Zombies(int zombieSpeed,int zombieHealth,int zombieDamage,int x,int y,ImageIcon image){
         this.zombieSpeed = zombieSpeed;
         this.zombieHealth = zombieHealth;
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
+        this.zombieDamage = zombieDamage;
+        this.xCoordinate = x;
+        this.yCoordinate = y;
         this.image = image;
         zombieCount++;
     }
