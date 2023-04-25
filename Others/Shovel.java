e63d5db1861be23d2590f557d0cd522f6155fa70
TuduPham <128670974+TuduPham@users.noreply.github.com>
2023-04-25 22:28:55 +0700

    Update Shovel.java

--- Others/Shovel.java
+++ Others/Shovel.java
@@ -1,15 +1,25 @@
 package Others;
 
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
 
 public class Shovel {
     private int xCoordinate;
     private int yCoordinate;
+    private boolean active = true;
+    private Point imageFirstPoint;
+    public Point imageCorner;
+    
+
     ImageIcon image = new ImageIcon("shovel.png");
 
     public Shovel(int x,int y){
         this.xCoordinate = x;
         this.yCoordinate = y;
+        imageCorner = new Point(x,y);
+        imageFirstPoint = new Point(x, y);
     }
 
     public ImageIcon getImage(){
@@ -24,4 +34,32 @@ public class Shovel {
         return yCoordinate;
     }
 
+    public boolean isActive(){
+        return active;
+    }
+
+    public void setActive(boolean active){
+        this.active = active;
+    }
+
+    public Point getImageFirstPoint(){
+        return imageFirstPoint;
+    }
+
+    public Point getImageCorner(){
+        return imageCorner;
+    }
+
+    public void setImageCorner(Point point){
+        this.imageCorner = point;
+    }
+
+    public boolean checkShovel(MouseEvent e){
+        if (e.getX() >= imageFirstPoint.getX() && e.getX() <= imageFirstPoint.getX() + image.getIconWidth()){
+            if (e.getY() >= imageFirstPoint.getY() && e.getY() <= imageFirstPoint.getY() + image.getIconHeight()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
