9f8a2c9ab640dc0cdbd7230f76d4fbcd073808b4
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 23:00:58 +0700

    Update Plants.java

--- Plants/Plants.java
+++ Plants/Plants.java
@@ -1,26 +1,114 @@
 package Plants;
 
+import java.awt.Point;
+
 import javax.swing.ImageIcon;
 
-public class PeaShooter extends Plants{
-    private static int countPeaShooter;
-    //public ArrayList<Pea> peaList;
-    private Pea pea;
+import Others.Projectile;
+
+public class Plants extends Projectile{
+    private int plantDamage;
+    private int plantHealth;
+    protected static int plantCount = 0;
+    public String name;
+    private int plantsValue;
    
-    public PeaShooter(int plantDamage, int plantHealth,int x,int y,int xBackyard,int yBackyard) {
-        super(plantDamage, plantHealth,x,y,new ImageIcon("pea_shooter.gif"),new ImageIcon("pea_shooter_price.png"),"PeaShooter",100,xBackyard,yBackyard);
-        //peaList = new ArrayList<Pea>();
-        ImageIcon image = new ImageIcon("pea_shooter_price.png");
-        pea = new Pea(plantDamage, x, y,x + image.getIconWidth()/2,y + image.getIconHeight());
-        countPeaShooter++;
+    private boolean checkDelay = false;
+    private int constDelay;
+    private int delay = 0;
+    private ImageIcon imageDelay;
+
+    public ImageIcon image;
+    public int WIDTH;
+    public int HEIGHT;
+    public ImageIcon cardImage;
+    private Point imageCorner;
+    public Point imageFirstPoint;
+    public Point currentPoint;
+    public int check = 0;
+
+    public Plants(int plantDamage,int plantHealth,int x,int y,ImageIcon image,ImageIcon cardImage,String name,int plantsValue,int xBackyard,int yBackyard,ImageIcon imageDelay,int constDelay){
+        super(x,y,0,image.getIconWidth(),image.getIconHeight(),xBackyard,yBackyard);
+        this.plantDamage = plantDamage;
+        this.plantHealth = plantHealth;
+        this.image = image;
+        this.cardImage = cardImage;
+        this.name = name;
+        this.imageCorner = new Point(x,y);
+        this.imageFirstPoint = new Point(x,y);
+        this.currentPoint = new Point(x,y);
+        this.plantsValue = plantsValue;
+        this.imageDelay = imageDelay;
+        this.constDelay = constDelay;
     }
 
-    public int getCountNormal(){
-        return countPeaShooter;
+    public int getDamage(){
+        return plantDamage;
     }
 
-    public Pea getPea(){
-        return pea;
+    public int getHealth(){
+        return plantHealth;
     }
 
+    public ImageIcon getImage(){
+        return image;
+    }
+
+    public int getPlantsValue(){
+        return plantsValue;
+    }
+
+    public Point getImageFirstPoint(){
+        return imageFirstPoint;
+    }
+
+    public Point getImageCorner(){
+        return imageCorner;
+    }
+
+    public void setImageCorner(Point point){
+        this.imageCorner = point;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ImageIcon getCardImage(){
+        return cardImage;
+    }
+
+    public void plantHit(int damage){
+        if (isImageActive()){
+            plantHealth -= damage;
+        }
+    }
+
+    public ImageIcon getImageDelay(){
+        return imageDelay;
+    }
+
+    public int getConstDelay(){
+        return constDelay;
+    }
+
+    public void setConstDelay(int delay){
+        this.constDelay = delay;
+    }
+
+    public int getDelay(){
+        return delay;
+    }
+
+    public void setDelay(int delay){
+        this.delay = delay;
+    }
+
+    public boolean getCheckDelay(){
+        return checkDelay;
+    }
+
+    public void setCheckDelay(boolean active){
+        this.checkDelay = active;
+    }
 }
