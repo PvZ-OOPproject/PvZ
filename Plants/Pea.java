a5d4a4f598589bf680395db6296482d0b8ee8671
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 23:00:12 +0700

    Update Pea.java

--- Plants/Pea.java
+++ Plants/Pea.java
@@ -6,55 +6,48 @@ import Others.Projectile;
 
 public class Pea extends Projectile{
     private int damage;
-    private int speed;
-    private int xCoordinate;
     private int xFirstCoordinate;
     private int yFirstCoordinate;
     private int delay = 850;
-    private int yCoordinate;
     private boolean shootActive = false;
     private boolean stop = false;
+    private boolean prepareStop = false;
     public static ImageIcon image = new ImageIcon("Pea.png");
     private static final int WIDTH = image.getIconWidth();
     private static final int HEIGHT = image.getIconHeight();
 
-    private int xBackyard;
-    private int yBackyard;    
-
     public Pea(int damage,int x,int y,int xBackyard,int yBackyard){
-        super(x,y,8,WIDTH,HEIGHT);
+        super(x,y,8,WIDTH,HEIGHT,xBackyard,yBackyard);
         this.damage = damage;
-        this.xCoordinate = x;
         this.xFirstCoordinate = x;
         this.yFirstCoordinate = y;
-        this.yCoordinate = y;
-        this.speed = 8;
-        this.xBackyard = xBackyard;
-        this.yBackyard = yBackyard;
     }
 
     public void updatePea() {
         if (!stop){
             if (isShootActive()){
-                if (xCoordinate < delay + xFirstCoordinate){
-                    xCoordinate += speed;
+                if (getXCoordinate() < delay + xFirstCoordinate){
+                    setXCoordinate(getXCoordinate() + getSpeed());
                     this.updatePos();
                 }
                 else{
-                    setX(xFirstCoordinate);
-                    setPos(xFirstCoordinate, yCoordinate);
-                    setActive(true);
+                    setXCoordinate(xFirstCoordinate);
+                    setPos(xFirstCoordinate, getYCoordinate());
+                    setImageActive(true);
                 }
             }
-            else{
-                    if (xCoordinate > xFirstCoordinate && xCoordinate < delay + xFirstCoordinate){
-                        xCoordinate += speed;
+            else{                
+                    if (getXCoordinate() > xFirstCoordinate && getXCoordinate() < delay + xFirstCoordinate){
+                        setXCoordinate(getXCoordinate() + getSpeed());
                         this.updatePos();
                     }
                     else{
-                        setX(xFirstCoordinate);
-                        setPos(xFirstCoordinate, yCoordinate);
-                        setActive(true);
+                        setXCoordinate(xFirstCoordinate);
+                        setPos(xFirstCoordinate, getYCoordinate());
+                        if (!getPrepareStop())
+                            setImageActive(true);
+                        else
+                            setStop(true);
                     }
             }
 
@@ -62,25 +55,9 @@ public class Pea extends Projectile{
         }
         else{
             setXFirstCoordinate(-200);
-            setPos(-200, yCoordinate);
-            setActive(false);
+            setPos(-200, getYCoordinate());
+            setImageActive(false);
         }
-    }
-
-    public int getX(){
-        return xCoordinate;
-    }
-
-    public void setX(int x){
-        xCoordinate = x;
-    }
-
-    public int getY(){
-        return yCoordinate;
-    }
-
-    public void setY(int y){
-        yCoordinate = y;
     }
 
     public int getDamage(){
@@ -118,8 +95,13 @@ public class Pea extends Projectile{
         this.shootActive = active;
     }
 
-    public int getYBackyard(){
-        return yBackyard;
+    public void setPrepareStop(boolean active){
+        this.prepareStop = active;
     }
+
+    public boolean getPrepareStop(){
+        return prepareStop;
+    }
+
 }
 
