0e931351b29210b726fdfeee91c2ffa91f2f8522
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 22:59:28 +0700

    Update Projectile.java

--- Others/Projectile.java
+++ Others/Projectile.java
@@ -4,12 +4,20 @@ import java.awt.geom.Rectangle2D;
 
 public class Projectile {
     private Rectangle2D.Float hitBox;
-    private boolean active = true;
+    private boolean imageActive = true;
     private int speed;
+    private int xBackyard;
+    private int yBackyard;
+    private int xCoordinate;
+    private int yCoordinate;
 
-    public Projectile(int x, int y,int speed,int width,int height){
+    public Projectile(int x, int y,int speed,int width,int height,int xBackyard,int yBackyard){
         hitBox = new Rectangle2D.Float(x,y,width,height);
         this.speed = speed;
+        this.xBackyard = xBackyard;
+        this.yBackyard = yBackyard;
+        this.xCoordinate = x;
+        this.yCoordinate = y;
     }
     public void updatePos(){
         hitBox.x += speed;
@@ -28,12 +36,39 @@ public class Projectile {
         return hitBox;
     }
 
-    public void setActive(boolean active){
-        this.active = active;
+    public void setImageActive(boolean active){
+        this.imageActive = active;
     }
 
-    public boolean isActive(){
-        return active;
+    public boolean isImageActive(){
+        return imageActive;
     }
     
+    public int getXBackyard(){
+        return xBackyard;
+    }
+    
+    public int getYBackyard(){
+        return yBackyard;
+    }
+
+    public void setXBackyard(int xBackyard){
+        this.xBackyard = xBackyard;
+    }
+
+    public void setYBackyard(int yBackyard){
+        this.yBackyard = yBackyard;
+    }
+
+    public int getXCoordinate(){
+        return xCoordinate;
+    }
+
+    public int getYCoordinate(){
+        return yCoordinate;
+    }
+
+    public void setXCoordinate(int x){
+        this.xCoordinate = x;
+    }
 }
