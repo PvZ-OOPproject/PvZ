3e940cbb885d1d9cb1b4631d1c3b8ede54be5296
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 23:00:31 +0700

    Update PeaShooter.java

--- Plants/PeaShooter.java
+++ Plants/PeaShooter.java
@@ -8,10 +8,8 @@ public class PeaShooter extends Plants{
     private Pea pea;
    
     public PeaShooter(int plantDamage, int plantHealth,int x,int y,int xBackyard,int yBackyard) {
-        super(plantDamage, plantHealth,x,y,new ImageIcon("pea_shooter.gif"),new ImageIcon("pea_shooter_price.png"),"PeaShooter",100,xBackyard,yBackyard);
-        //peaList = new ArrayList<Pea>();
-        ImageIcon image = new ImageIcon("pea_shooter_price.png");
-        pea = new Pea(plantDamage, x, y,x + image.getIconWidth()/2,y + image.getIconHeight());
+        super(plantDamage, plantHealth,x,y,new ImageIcon("pea_shooter.gif"),new ImageIcon("pea_shooter_price.png"),"PeaShooter",100,xBackyard,yBackyard,new ImageIcon("sun_flower_price.png"),250);
+        pea = new Pea(plantDamage, x +  3*getImage().getIconWidth()/4, y + getImage().getIconHeight()/4,xBackyard,yBackyard);
         countPeaShooter++;
     }
 
