10fe42987397cfb42d5686935764fb0561770623
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 23:02:54 +0700

    Update walnut.java

--- Plants/walnut.java
+++ Plants/walnut.java
@@ -2,11 +2,11 @@ package Plants;
 
 import javax.swing.ImageIcon;
 
-public class walnut extends Plants{
+public class Walnut extends Plants{
     private static int countSunFlower;
    
-    public walnut(int plantDamage, int plantHealth,int x,int y,int xBackyard,int yBackyard) {
-        super(plantDamage, plantHealth,x,y,new ImageIcon("walnut_full_life.gif"),new ImageIcon("walnut_full_life.gif"),"Walnut",50,xBackyard,yBackyard);
+    public Walnut(int plantDamage, int plantHealth,int x,int y,int xBackyard,int yBackyard) {
+        super(plantDamage, plantHealth,x,y,new ImageIcon("walnut_full_life.gif"),new ImageIcon("walnut_full_life.gif"),"Walnut",50,xBackyard,yBackyard,new ImageIcon("walnut_full_life.gif"),250);
         countSunFlower++;
     }
 
