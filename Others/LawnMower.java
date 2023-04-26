602e9f93e1213d01ef81f82916eb5184d929d4bb
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-04-26 22:58:31 +0700

    Update LawnMower.java

--- Others/LawnMower.java
+++ Others/LawnMower.java
@@ -3,40 +3,28 @@ package Others;
 import javax.swing.ImageIcon;
 
 public class LawnMower extends Projectile{
-    private int xCoordinate;
-    private int yCoordinate;
     static ImageIcon image = new ImageIcon("lawn_mower.gif");
     private static final int WIDTH = image.getIconWidth();
     private static final int HEIGHT = image.getIconHeight();
 
-    public LawnMower(int x,int y){
-        super(x - WIDTH/2,y - HEIGHT/2,10,WIDTH,HEIGHT);
-        this.xCoordinate = x - WIDTH/2;
-        this.yCoordinate = y - HEIGHT/2;
+    public LawnMower(int x,int y,int xBackyard,int yBackyard){
+        super(x - WIDTH/2,y - HEIGHT/2,10,WIDTH,HEIGHT,xBackyard,yBackyard);
     }
 
     public void updateLawnMower(){
-        if(!isActive()){
-            if (xCoordinate <= 1000){
-                xCoordinate += this.getSpeed();
-                this.updatePos();
+        if(!isImageActive()){
+            if (getXCoordinate() <= 1000){
+                setXCoordinate(getXCoordinate() + getSpeed());
+                updatePos();
             }
             else
-                this.setPos(0, yCoordinate);
+                setPos(0, getYCoordinate());
 
         }
     }
 
     public ImageIcon getImage(){
         return image;
-    }
-
-    public int getXCoordinate(){
-        return xCoordinate;
-    }
-
-    public int getYCoordinate(){
-        return yCoordinate;
     }
 
 }
