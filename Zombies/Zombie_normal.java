bb131c4ebb1e7962ca07f3fa4f9fb22b9ed1cacb
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-05-06 12:01:15 +0700

    Update Zombie_normal.java

--- Zombies/Zombie_normal.java
+++ Zombies/Zombie_normal.java
@@ -3,17 +3,11 @@ package Zombies;
 import javax.swing.ImageIcon;
 
 public class Zombie_normal extends Zombies{
-    private static int countNormal;
-    public static ImageIcon image = new ImageIcon("zombie_normal.gif");
    
-    public Zombie_normal(int zombieSpeed, int zombieHealth, int xCoordinate, int yCoordinate,int xBackyard,int yBackyard) {
-        super(zombieSpeed, zombieHealth, 1 ,xCoordinate, yCoordinate,image,xBackyard,yBackyard);
-        countNormal++;
+    public Zombie_normal(double zombieSpeed, int zombieHealth, int zombieDamage ,double xCoordinate, double yCoordinate,int xBackyard,int yBackyard) {
+        super(zombieSpeed, zombieHealth, zombieDamage ,xCoordinate, yCoordinate,new ImageIcon("zombie_normal.gif"),xBackyard,yBackyard);
     }
 
-    public int getCountNormal(){
-        return countNormal;
-    }
 
 
 }
